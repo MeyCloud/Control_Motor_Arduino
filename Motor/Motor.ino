3697fbf8466f985883ea92ee91d5a2f2bf4840b9
DEYVI28 <danimaksilva28@gmail.com>
2025-05-26 02:07:31 -0500

    Actualización: Mejoras en el control del motor y refactorización del código

---
+++ Motor/Motor.ino
@@ -0,0 +1,39 @@
+// Pines de control
+const int AIN1 = 2;
+const int AIN2 = 3;
+const int PWMA = 5;  // PWM para velocidad
+const int STBY = 4;
+
+void setup() {
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(STBY, OUTPUT);
+
+  // Activar el driver
+  digitalWrite(STBY, HIGH);
+}
+
+void loop() {
+  // Gira el motor hacia adelante a velocidad media
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(PWMA, 180);  // Velocidad (0 a 255) PWM MODIFICADO
+
+  delay(3000);  // 3 segundos
+
+  // Detener el motor
+  analogWrite(PWMA, 0);
+  delay(1000);
+
+  // Gira el motor hacia atrás
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  analogWrite(PWMA, 200);  // PWM MODIFICADOS
+
+  delay(3000);
+
+  // Parar el motor
+  analogWrite(PWMA, 0);
+  delay(1000);
+}
